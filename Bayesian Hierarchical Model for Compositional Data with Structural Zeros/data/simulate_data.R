6b54145055578d9a4401207f0a12f84c5b7bb21c
Catherine Holland <75622719+catherineholland1@users.noreply.github.com>
2025-05-29 09:20:13 +0100

    Add files via upload

--- Bayesian Hierarchical Model for Compositional Data with Structural Zeros/data/simulate_data.R
+++ Bayesian Hierarchical Model for Compositional Data with Structural Zeros/data/simulate_data.R
@@ -17,17 +17,17 @@ setwd("~/Library/CloudStorage/OneDrive-UniversityofGlasgow/Documents/Statistical
 
 ## PARAMETERS ##
 
-n_items <- 100
+n_items <- 250
 n_rep <- 4
 types <- 1:5
 
-n_elements <- 10
+n_elements <- 5
 
 # Create a data frame to hold results
 simulated_data <- data.frame()
 
 # Simulate function
-simulate_compositions <- function(n = 1, d, zero_prob = 0.3) {
+simulate_compositions <- function(n = 1, d) {
   
   # n = number of samples
   # d = number of components
@@ -36,15 +36,21 @@ simulate_compositions <- function(n = 1, d, zero_prob = 0.3) {
   compositions <- matrix(0, nrow = n, ncol = d)
   
   for (i in 1:n) {
+    
+    # Randomly choose the prob of zero
+    zero_prob <- c(0, rbeta(d - 1, shape1 = 0.5, shape2 = 0.5))
+    
     vals <- runif(d, min = 0, max = 1)
     # Randomly set some components to zero
     zero_indices <- runif(d) < zero_prob
+    zero_indices[1] <- FALSE
     vals[zero_indices] <- 0
     
     # If all zeros, resample to avoid division by zero
     while (sum(vals) == 0) {
       vals <- runif(d, min = 0, max = 1)
       zero_indices <- runif(d) < zero_prob
+      zero_indices[1] <- FALSE
       vals[zero_indices] <- 0
     }
     
@@ -62,7 +68,7 @@ for (item_id in 1:n_items) {
   
   for (rep_id in 1:n_rep) {
     # Generate one compositional vector (1 x n_elements)
-    comp <- simulate_compositions(n = 1, d = n_elements, zero_prob = 0.1)
+    comp <- simulate_compositions(n = 1, d = n_elements)
     comp_vec <- as.numeric(comp)
     
     variables <- data.frame(setNames(as.list(comp_vec), paste0("variable_", 1:n_elements)))
@@ -80,9 +86,13 @@ for (item_id in 1:n_items) {
 
 head(simulated_data)
 
-rowSums(simulated_data[, 4:13])
+rowSums((simulated_data %>% select(starts_with('variable'))))
 
-sum(simulated_data[, 4:13] == 0)
+sum((simulated_data %>% select(starts_with('variable'))) == 0)
+sum((simulated_data %>% select(starts_with('variable'))) == 0) / (nrow(simulated_data)*n_elements)
+
+apply(simulated_data %>% select(starts_with('variable')), 2, function(x) sum(x == 0))
+apply(simulated_data %>% select(starts_with('variable')), 2, function(x) sum(x == 0)) / nrow(simulated_data)
 
 
 ##################################.
