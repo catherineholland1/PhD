279755375cbd81eecc6bcfe2219dc22eb2af459d
Catherine Holland <75622719+catherineholland1@users.noreply.github.com>
2025-05-29 13:52:43 +0100

    Add files via upload

--- Bayesian Hierarchical Model for Compositional Data with Structural Zeros/data/simulate_data.R
+++ Bayesian Hierarchical Model for Compositional Data with Structural Zeros/data/simulate_data.R
@@ -6,6 +6,8 @@
 
 library(dplyr)
 library(readr)
+library(MASS)
+library(tidyverse)
 
 ##################################.
 
@@ -17,79 +19,69 @@ setwd("~/Library/CloudStorage/OneDrive-UniversityofGlasgow/Documents/Statistical
 
 ## PARAMETERS ##
 
-n_items <- 250
+n_items <- 500
 n_rep <- 4
 types <- 1:5
 
-n_elements <- 5
+n_components <- 5
 
-# Create a data frame to hold results
-simulated_data <- data.frame()
-
-# Simulate function
-simulate_compositions <- function(n = 1, d) {
-  
-  # n = number of samples
-  # d = number of components
-  # zero_prob = probability a component is zero
-  
-  compositions <- matrix(0, nrow = n, ncol = d)
-  
-  for (i in 1:n) {
-    
-    # Randomly choose the prob of zero
-    zero_prob <- c(0, rbeta(d - 1, shape1 = 0.5, shape2 = 0.5))
-    
-    vals <- runif(d, min = 0, max = 1)
-    # Randomly set some components to zero
-    zero_indices <- runif(d) < zero_prob
-    zero_indices[1] <- FALSE
-    vals[zero_indices] <- 0
-    
-    # If all zeros, resample to avoid division by zero
-    while (sum(vals) == 0) {
-      vals <- runif(d, min = 0, max = 1)
-      zero_indices <- runif(d) < zero_prob
-      zero_indices[1] <- FALSE
-      vals[zero_indices] <- 0
-    }
-    
-    # Normalize so sum = 1
-    compositions[i, ] <- vals / sum(vals)
-  }
-  
-  return(compositions)
-}
+total = 100
 
 
-## SIMULATE ##
-for (item_id in 1:n_items) {
-  item_type <- sample(types, 1)
+## SIMULATE DATA ##
+
+item_effects <- mvrnorm(n_items, mu = rep(0, n_components - 1), Sigma = diag(0.5, n_components - 1))
+
+## Decide which components are non-zero per item ##
+# Always keep variable_1 (index 1), others randomly dropped
+non_zero_components_list <- map(1:n_items, function(i) {
+  keep_idx <- sample(2:n_components, sample(1:(n_components - 1), 1))  # pick others to keep
+  sort(unique(c(1, keep_idx)))  # always keep variable_1
+})
+
+## SIMULATE OBSERVATIONS ##
+simulated_data <- map_df(1:n_items, function(item_id) {
+  keep_idx <- non_zero_components_list[[item_id]]
   
-  for (rep_id in 1:n_rep) {
-    # Generate one compositional vector (1 x n_elements)
-    comp <- simulate_compositions(n = 1, d = n_elements)
-    comp_vec <- as.numeric(comp)
+  map_df(1:n_rep, function(obs_id) {
+    noise <- rnorm(n_components - 1, 0, 0.3)
+    log_ratios <- item_effects[item_id, ] + noise
+    log_ratios <- c(log_ratios, 0)  # last component anchored
     
-    variables <- data.frame(setNames(as.list(comp_vec), paste0("variable_", 1:n_elements)))
+    masked_log_ratios <- rep(-Inf, n_components)
+    masked_log_ratios[keep_idx] <- log_ratios[keep_idx]
     
-    # Add new row to simulated_data
-    simulated_data <- rbind(simulated_data, data.frame(
-      type = item_type,
+    exp_vals <- exp(masked_log_ratios)
+    props <- exp_vals / sum(exp_vals)
+    comps <- round(props * total, 2)
+    
+    tibble(
       item = item_id,
-      replicate = rep_id,
-      variables
-    ))
-  }
-}
+      rep = obs_id,
+      variable_1 = comps[1],
+      variable_2 = comps[2],
+      variable_3 = comps[3],
+      variable_4 = comps[4],
+      variable_5 = comps[5]
+    )
+  })
+})
 
+
+simulated_data <- simulated_data %>%
+  group_by(item) %>%
+  mutate(type = sample(1:5, 1, replace = T), .after = 'item') %>% 
+  ungroup()
+
+
+## CHECK ##
 
 head(simulated_data)
 
 rowSums((simulated_data %>% select(starts_with('variable'))))
 
 sum((simulated_data %>% select(starts_with('variable'))) == 0)
-sum((simulated_data %>% select(starts_with('variable'))) == 0) / (nrow(simulated_data)*n_elements)
+sum((simulated_data %>% select(starts_with('variable'))) == 0) / (nrow(simulated_data)*n_components)
 
 apply(simulated_data %>% select(starts_with('variable')), 2, function(x) sum(x == 0))
 apply(simulated_data %>% select(starts_with('variable')), 2, function(x) sum(x == 0)) / nrow(simulated_data)
